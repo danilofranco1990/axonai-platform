a05325dee57661a4c43e984e09b0915101c4272e
Danilo Franco <danilo.franco90@gmail.com>
2025-09-04 00:04:22 -0300

    feat: Criado o projeto

---
+++ src/main/java/com/axonai/platform/AxonaiPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.axonai.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonaiPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxonaiPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axonai/platform/AxonaiPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axonai.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxonaiPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
