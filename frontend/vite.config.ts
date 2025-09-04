8fb92d1fdd142e80680c55e9742eb8acd94e9772
Danilo Franco <danilo.franco90@gmail.com>
2025-09-04 14:59:33 -0300

    fix(test): Mock static assets and clean up test file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,5 +10,14 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+    // Adicione esta seção para mocar os imports de CSS
+    css: true, 
+  },
+  // Adicione esta seção para mocar os imports de imagens
+  resolve: {
+    alias: {
+      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+        new URL('./tests/fileMock.ts', import.meta.url).pathname,
+    },
   },
 });
