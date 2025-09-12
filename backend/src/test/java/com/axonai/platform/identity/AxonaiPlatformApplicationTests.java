2ac31539de17db1aaa5a9f1f9e538061f737aac2
Danilo Franco <danilo.franco90@gmail.com>
2025-09-12 17:33:50 -0300

    chore: deletaado testes

--- backend/src/test/java/com/axonai/platform/identity/AxonaiPlatformApplicationTests.java
+++ backend/src/test/java/com/axonai/platform/identity/AxonaiPlatformApplicationTests.java
@@ -1,20 +1,10 @@
 package com.axonai.platform.identity;
 
-import com.axonai.platform.identity.application.port.out.JwtProvider;
-import com.axonai.platform.identity.application.port.out.UserRepositoryPort;
-import com.axonai.platform.identity.domain.service.PasswordPolicy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class AxonaiPlatformApplicationTests extends PostgresContainerConfig {
-
-    @MockitoBean private UserRepositoryPort userRepositoryPort;
-
-    @MockitoBean private PasswordPolicy passwordPolicy;
-
-    @MockitoBean private JwtProvider jwtProvider;
 
     @Test
     void contextLoads() {}
